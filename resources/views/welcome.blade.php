3108ab588b1cc3c5e33973c7b7785797f9d58b87
chidoriii7 <muratore.danilo@yahoo.it>
2024-07-09 23:33:11 +0200

    [upd] milestone 7 + bonus

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -7,6 +7,23 @@
     @vite('resources/js/app.js')
 </head>
 <body>
-    
+    <div class="container py-5">
+        <div class="row py-5">
+            @foreach ($movies as $movie)
+            <div class="col-4 text-center py-4">
+                <ul>
+                    <div>
+                        <div class="card">
+                            <li class="movie-title">Title: {{ $movie->original_title }}</li>
+                            <li class="movie-origin">Origin:{{ $movie->nationality }}</li>
+                            <li class="movie-date">Date: {{ $movie->date }}</li>
+                            <li class="movie-vote">Vote: {{ $movie->vote }}</li>
+                        </div>
+                    </div>
+                </ul>
+            </div>
+            @endforeach
+        </div>
+    </div>
 </body>
 </html>
